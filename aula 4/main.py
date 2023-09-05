95f34ef8c897ea82a1d593ca2871a21d5edcbb5a
André Ferreira Rodrigues <130712797+Jozincanela@users.noreply.github.com>
2023-09-05 14:44:37 -0300

    Aula 4
    Aula 4 Fastapi, primeira API Rest. Feita com Python e Html

---
+++ aula 4/main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from apiTools import *
+
+app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+
+@app.get("/covid", response_class=HTMLResponse)
+def read_root(request: Request): 
+    url = 'https://covid19-brazil-api.vercel.app/api/report/v1'
+    dados_covid = get_data_url(url)
+
+    return templates.TemplateResponse(
+        'covid.html',
+        {"request": request, 'dados_covid': dados_covid }
+    )
+
+@app.get("/covid/desc", response_class=HTMLResponse)
+def read_desc(request: Request):
+    url = 'https://covid19-brazil-api.vercel.app/api/report/v1'
+    dados_covid = get_data_url(url)
+    organized_desc = organize_case_list(dados_covid)
+
+    return templates.TemplateResponse(
+        'desc.html',
+        {"request": request, 'organized_desc': organized_desc }
+    )
+
+@app.get("/covid/cresc", response_class=HTMLResponse)
+def read_cresc(request: Request):
+    url = 'https://covid19-brazil-api.vercel.app/api/report/v1'
+    dados_covid = get_data_url(url)
+    organized_cresc = organize_case_list_reverse(dados_covid)
+
+    return templates.TemplateResponse(
+        'cresc.html',
+        {"request": request, 'organized_cresc': organized_cresc }
+    )
+
+
+@app.get("/covid/mortes_desc", response_class=HTMLResponse)
+def read_deaths_desc(request: Request):
+    url = 'https://covid19-brazil-api.vercel.app/api/report/v1'
+    dados_covid = get_data_url(url)
+    organized_deaths_desc = organize_deaths_list(dados_covid)
+
+    return templates.TemplateResponse(
+        'desc_mortes.html',
+        {"request": request, 'organized_deaths_desc': organized_deaths_desc }
+    )
+    
+    
+
+@app.get("/covid/mortes_cresc", response_class=HTMLResponse)
+def read_deaths_cresc(request: Request):
+    url = 'https://covid19-brazil-api.vercel.app/api/report/v1'
+    dados_covid = get_data_url(url)
+    organized_deaths_cresc = organize_deaths_list_reverse(dados_covid)
+
+    return templates.TemplateResponse(
+        'cresc_mortes.html',
+        {"request": request, 'organized_deaths_cresc': organized_deaths_cresc }
+    )
